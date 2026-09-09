06541a524fb8eeb38be95469b0a721068c6c8fc3
Fardin Haque <fardinhaque.work@gmail.com>
2026-09-09 09:56:04 +0600

    Bundle complete PyInstaller output in AppImage

--- build_linux.sh
+++ build_linux.sh
@@ -4,7 +4,8 @@ PYTHON_BIN="${PYTHON_BIN:-python3}"
 export PYINSTALLER_CONFIG_DIR="${PYINSTALLER_CONFIG_DIR:-$PWD/.pyinstaller}"
 "$PYTHON_BIN" -m PyInstaller --noconfirm --clean --paths src --name "PKG Viewer" src/pkgviewer/launcher.py
 mkdir -p dist/appimage/AppDir/usr/bin dist/appimage/AppDir/usr/share/applications
-cp "dist/PKG Viewer/PKG Viewer" dist/appimage/AppDir/usr/bin/pkgviewer
+cp -R "dist/PKG Viewer/." dist/appimage/AppDir/usr/bin/
+mv "dist/appimage/AppDir/usr/bin/PKG Viewer" dist/appimage/AppDir/usr/bin/pkgviewer
 chmod +x dist/appimage/AppDir/usr/bin/pkgviewer
 cat > dist/appimage/AppDir/pkgviewer.svg <<'EOF'
 <svg xmlns="http://www.w3.org/2000/svg" width="256" height="256" viewBox="0 0 256 256">
